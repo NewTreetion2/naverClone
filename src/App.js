15960af9bd37fb1fc8443c3fd0a1e6512aa893cf
NewTreetion2 <115389778+NewTreetion2@users.noreply.github.com>
2023-06-12 23:46:02 +0900

    0612 네이버 클론코딩 시작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <p>Typescript 연습을 위한 Naver 클론코딩</p>;
 }
 
 export default App;
